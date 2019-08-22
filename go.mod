170eaa197824df857e8259f96030d4475aff3a73
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-08-22 13:42:09 +0200

    Add implementation

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module logur.dev/integration/logr
 
 go 1.12
 
-require github.com/goph/logur v0.12.0
+require (
+	github.com/go-logr/logr v0.1.0
+	github.com/goph/logur v0.12.0
+)
--- logger_test.go
+++ logger_test.go
@@ -1,1 +1,15 @@
 package logr
+
+import (
+	"testing"
+
+	"github.com/go-logr/logr"
+)
+
+func TestLogger(t *testing.T) {
+	var logger interface{} = New(nil)
+
+	if _, ok := logger.(logr.Logger); !ok {
+		t.Error("Logger does not implement the logr.Logger interface")
+	}
+}
